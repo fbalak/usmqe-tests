882dda9151c37085703da6468b4afb4401ae329a
Filip Balák <fbalak@redhat.com>
2017-03-20 12:54:14 -0400

    add conftest.py for api containing asserts for error responses

---
+++ usmqe_tests/api/conftest.py
@@ -0,0 +1,26 @@
+import pytest
+
+import json
+
+
+@pytest.fixture
+def not_found_response():
+    """Generate asserts for ``404 Not found`` response."""
+
+    return {
+        "json": json.loads('{"Error": "Not found"}'),
+        "cookies": None,
+        "ok": False,
+        "reason": 'Not Found',
+        "status": 404}
+
+@pytest.fixture
+def unauthorized_response():
+    """Generate asserts for ``401 Unauthorized`` response."""
+
+    return {
+        "json": json.loads('{"Error": "Unauthorized"}'),
+        "cookies": None,
+        "ok": False,
+        "reason": 'Unauthorized',
+        "status": 401}
